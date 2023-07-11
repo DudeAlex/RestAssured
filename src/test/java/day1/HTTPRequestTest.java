a7ead54c6b41b4967a8c6211a4d1ef19e662ebd9
AlexLeoDude <dudealexlion@gmail.com>
2023-07-11 16:13:45 -0700

    added createUser test

--- src/test/java/day1/HTTPRequestTest.java
+++ src/test/java/day1/HTTPRequestTest.java
@@ -1,6 +1,9 @@
 package day1;
 
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
@@ -16,6 +19,23 @@ public class HTTPRequestTest {
         .then()
                 .statusCode(200)
                 .body("page", equalTo(2))
+                .log().all();
+    }
+
+    @Test
+    void createUser(){
+        HashMap<String, String> data = new HashMap<>();
+        data.put("name", "Alex");
+        data.put("job", "trainer");
+
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(data)
+        .when()
+                .post("https://reqres.in/api/users")
+        .then()
+                .statusCode(201)
                 .log().all();
     }
 }
